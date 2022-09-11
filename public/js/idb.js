9182652cb8dc638e5020c38ac02eb09f2c4348fe
Stella Sun <sunshuyu0413@163.com>
2022-09-11 17:38:23 -0400

    offline function & get all function

--- public/js/idb.js
+++ public/js/idb.js
@@ -13,7 +13,7 @@ request.onsuccess = (event) => {
 
   if (navigator.onLine) {
     checkDatabase();
-  };
+  }
 };
 
 request.onerror = (err) => {
@@ -21,3 +21,27 @@ request.onerror = (err) => {
 };
 
 // submit without internet connection
+function saveRecord(record) {
+  const transaction = db.transaction("new_transaction", "readwrite");
+  const store = transaction.objectStore("new_trasaction");
+  store.add(record);
+}
+
+getAll.onsuccess = function() {
+    if (getAll.result.length > 0) {
+        fetch('/api/transaction', {
+            method: 'POST',
+            body: JSON.stringify(getAll.result),
+            headers: {
+                Accept: 'application/json, text/plain, */*',
+                'Content-Type': 'application/json'
+            }
+        })
+            .then(response => response.json())
+            .then(() => {
+                const transaction = db.transaction("new_trasaction", "readwrite");
+                const store = transaction.objectStore("new_trasaction");
+                store.clear();
+            });
+        }
+    };
